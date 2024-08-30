f38e6b36ecf46aa4b0fe29d74fb2512545fcc8fb
David Case <david@fyxgaming.com>
2024-08-30 16:54:05 -0400

    blockHeaderBytes

--- src/models/block-header.ts
+++ src/models/block-header.ts
@@ -1,9 +1,23 @@
+import { Utils } from "@bsv/sdk";
+
 export interface BlockHeader {
   hash: string;
   height: number;
   prevHash: string;
   time: number;
+  version: number;
   merkleroot: string;
   bits: number;
   nonce: number;
+}
+
+export function blockHeaderBytes(header: BlockHeader): number[] {
+  const writer = new Utils.Writer();
+  writer.writeInt32LE(header.version);
+  writer.write([...Buffer.from(header.prevHash, "hex").reverse()])
+  writer.write([...Buffer.from(header.merkleroot, "hex").reverse()]);
+  writer.writeUInt32LE(header.time);
+  writer.write(Utils.toArray(header.bits, "hex"));
+  writer.writeUInt32LE(header.nonce);
+  return writer.toArray();
 }
